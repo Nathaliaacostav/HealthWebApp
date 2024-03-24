67985ef4b791b6480e2cc455927ca9852b9b5457
Nathalia Acosta Vélez <nathalia285@gmail.com>
2024-03-24 15:31:25 -0500

    maquetados y estilos completos interfaz PaymentMethod

--- healthWebApp/src/pages/PaymentMethod/index.jsx
+++ healthWebApp/src/pages/PaymentMethod/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from "../../components/Footer";
 import { FiMoreVertical } from 'react-icons/fi';
 import { IoIosArrowBack } from "react-icons/io";
+import { IoWalletOutline } from "react-icons/io5"
 import { useNavigate } from 'react-router-dom';
 import './styles.sass';
 
@@ -15,7 +16,7 @@ const PaymentMethod = () => {
     <main className='PaymentMethod'>
       <div className="PaymentMethod__topBar">
         <button className="PaymentMethod__backButton" onClick={goTo}>
-        <IoIosArrowBack />
+          <IoIosArrowBack />
         </button>
         <h3 className="PaymentMethod__pageDescription">Transacción</h3>
         <button className="PaymentMethod__fi-more">
@@ -26,27 +27,31 @@ const PaymentMethod = () => {
         <h1 className='PaymentMethod__container-title1'>Total a pagar</h1>
         <h1 className='PaymentMethod__container-title2'>$15.000</h1>
         <p className='PaymentMethod__container-paragraph'>Valor en pesos colombianos</p>
-        <small>Escoge tu método de pago</small>
-        <div className='PaymentMethod__container-Method'>
-          <div><img src="" alt="" /></div>
-          <div><img src="" alt="" /></div>
+        <p className='PaymentMethod__container-small'>¡Escoge tu método de pago!</p>
+        <div className='PaymentMethod__container-method'>
+          <button className='type'>
+            <img className='iconPay'src="https://res.cloudinary.com/dhhyc88td/image/upload/v1711301781/pseLogo_zifxqp.png" alt="Descripción de la imagen" />
+          </button>
+          <button className='type'>
+            <img className='iconPay2'src="https://res.cloudinary.com/dhhyc88td/image/upload/v1711301839/tarjeta-de-credito_rthqip.png" alt="" />
+          </button>
         </div>
-        <div>
-          <button>Tu bolsillo</button>
+        <div className='PaymentMethod__container-bolsillo'>
+          <button className='bolsilloStyle'> <IoWalletOutline /> Tu bolsillo</button>
         </div>
-        <h3>Card info</h3>
-        <div>
+        <h3 className='PaymentMethod__container-subtitle'>Card info</h3>
+        <div className='PaymentMethod__container-dataPay'>
           <div>
-            <p>Número</p>
+            <p className='paragraph2'>Número</p>
           </div>
           <div>
-            <p>Nombre</p>
+            <p className='paragraph2'>Nombre</p>
           </div>
-          <div>
-            <p>Fecha de exp.</p>
-            <p>Código</p>
+          <div className='container3'>
+            <p className='paragraph2'>Fecha de exp.</p>
+            <p className='paragraph2'>Código</p>
           </div>
-          <button>Continuar</button>
+          <button className='PaymentMethod__container-Btn'>Continuar</button>
         </div>
       </section>
       <Footer />
